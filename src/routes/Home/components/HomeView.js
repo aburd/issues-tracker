24f27c5ceddac6c2dd6ca811b184fcb3281f3111
aburd <burdick.aaron@gmail.com>
2017-05-19 01:40:22 +0900

    Set up basic actions for changing pages and ajaxing data from github api Changes to Readme.md

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import RepoLinks from '../../../components/RepoLinks'
 import DuckImage from '../assets/Duck.jpg'
 import './HomeView.scss'
 
 export const HomeView = () => (
   <div>
-    <h4>Welcome!</h4>
-    <img alt='This is a duck, because Redux!' className='duck' src={DuckImage} />
+    <p>レポジトリーを選択してください。</p>
+    <RepoLinks />
   </div>
 )
 
