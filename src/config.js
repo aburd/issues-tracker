f38551e0681b7946dec412b82ceb903429f794c8
aburd <burdick.aaron@gmail.com>
2017-05-19 01:37:09 +0900

    Make a config file to keep track of the different repos

---
+++ src/config.js
@@ -0,0 +1,14 @@
+export const SETTINGS = {
+  title: 'Github Issues Tracker',
+  apiBase: 'https://api.github.com'
+}
+export const REPOS = {
+  protractor: {
+    url: '/repos/angular/protractor',
+    name: 'Angular-Protractor'
+  },
+  'react-redux-starter-kit': {
+    url: '/davezuko/react-redux-starter-kit',
+    name: 'React-Redux-Starter-Kit'
+  }
+}
