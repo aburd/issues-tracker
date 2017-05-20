fb1c5bde8ca13d81af049170127947c5d40d7a43
aburd <burdick.aaron@gmail.com>
2017-05-20 15:42:48 +0900

    Add Loader animation to project

---
+++ src/components/Loader/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Loader.scss'
+
+export const Loader = () => (
+  <div id='loader'>
+    <span />
+    <span />
+    <span />
+    <span />
+    <span />
+    <span />
+    <span />
+    <span />
+  </div>
+)
+
+export default Loader
